87e315f96daafd32c8dfeb4fd29c54148fb261fc
David <davidoregan91@gmail.com>
2019-12-31 22:47:12 +0000

    feat(header): more tests

--- components/organisms/Footer/index.vue
+++ components/organisms/Footer/index.vue
@@ -6,7 +6,7 @@
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
               <div class="footer-logo">
-                <img src="img/footer-logo.png" alt="" />
+                <img src="https://nuxtjs.org/logos/nuxt.svg" alt="" />
               </div>
             </div>
 
--- test/components/Header/Header.spec.js
+++ test/components/Header/Header.spec.js
@@ -6,10 +6,17 @@ const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
 describe('Header', () => {
-  test('is a Vue instance', () => {
+  it('is a Vue instance', () => {
     const wrapper = mount(Header, {
       localVue
     })
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('is renders a nuxt logo with a link', () => {
+    const wrapper = mount(Header, {
+      localVue
+    })
+    expect(wrapper.find('.navbar-brand').attributes('href')).toBe('https://nuxtjs.org')
   })
 })
