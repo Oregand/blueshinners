26b4b7bf778ad34a9bdc9149dfd28d392a4eda03
David <davidoregan91@gmail.com>
2020-01-13 15:10:56 +0000

    feat(CMS): update cms for memorials

--- pages/memorial/index.vue
+++ pages/memorial/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <section id="testimonial" class="section">
-    <div />
-  </section>
+  <div>
+    <Header />
+    <section id="testimonial" class="section">
+      <div />
+    </section>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from '~/components/organisms/Header'
+import Footer from '~/components/organisms/Footer'
+
 export default {
+  components: {
+    Header,
+    Footer
+  },
   computed: {
     memorialPosts() {
       return this.$store.state.memorialPosts
