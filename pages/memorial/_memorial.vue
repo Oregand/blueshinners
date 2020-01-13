fb48e7f0b24ad4dd687408ca8b54cd176b0167b7
David <davidoregan91@gmail.com>
2020-01-13 16:47:00 +0000

    feat(CMS): update cms for memorials

--- pages/memorial/_memorial.vue
+++ pages/memorial/_memorial.vue
@@ -1,14 +1,36 @@
 <template>
-  <section id="testimonial" class="section">
-    <article>
-      <h1>{{ memorialPost.title }}</h1>
-      <div v-html="$md.render(memorialPost.body)" />
-    </article>
-  </section>
+  <div>
+    <Header />
+    <section class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 pl-4">
+            <div class="business-item-info">
+              <h3>{{ memorialPost.title }}</h3>
+              <div v-html="$md.render(memorialPost.body)" />
+              <a
+                class="btn btn-common"
+                href="https://fitnessvolt.com/35656/james-shinners-passed-away/"
+                >Learn More</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from '~/components/organisms/Header'
+import Footer from '~/components/organisms/Footer'
+
 export default {
+  components: {
+    Header,
+    Footer
+  },
   async asyncData({ params, payload }) {
     if (payload) return { memorialPost: payload }
     else
